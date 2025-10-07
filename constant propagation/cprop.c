4405551cd17cfc27c84ca029d85a23fee12659fc
Giribala Arun <120817536+Giga4byte@users.noreply.github.com>
2025-10-07 22:17:43 +0530

    Update cprop.c

--- constant propagation/cprop.c
+++ constant propagation/cprop.c
@@ -86,3 +86,71 @@ int main() {
     return 0;
 }
 
+
+/*
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAX 26  // for variables a-z
+
+char values[MAX][100];  // values of variables
+int isConst[MAX];       // is the variable a constant?
+
+int varIndex(char var) {
+    return var - 'a';
+}
+
+void propagate(char *expr, char *out) {
+    int i = 0, k = 0;
+    while (expr[i]) {
+        if (isalpha(expr[i])) {
+            int idx = varIndex(expr[i]);
+            if (isConst[idx]) {
+                strcpy(&out[k], values[idx]);
+                k += strlen(values[idx]);
+            } else {
+                out[k++] = expr[i];
+            }
+        } else {
+            out[k++] = expr[i];
+        }
+        i++;
+    }
+    out[k] = '\0';
+}
+
+int main() {
+    int n;
+    char stmt[100], lhs, rhs[100], newRhs[100];
+
+    printf("Enter number of statements: ");
+    scanf("%d", &n);
+
+    printf("Enter statements (e.g., a=5, b=a):\n");
+
+    for (int i = 0; i < n; i++) {
+        scanf("%s", stmt);
+        lhs = stmt[0];
+        strcpy(rhs, stmt + 2);  // skip 'a='
+
+        propagate(rhs, newRhs);
+        printf("%c = %s\n", lhs, newRhs);
+
+        int idx = varIndex(lhs);
+        strcpy(values[idx], newRhs);
+
+        // Mark as constant if it's purely a number
+        int isNum = 1;
+        for (int j = 0; newRhs[j]; j++) {
+            if (!isdigit(newRhs[j])) {
+                isNum = 0;
+                break;
+            }
+        }
+        isConst[idx] = isNum;
+    }
+
+    return 0;
+}
+*/
